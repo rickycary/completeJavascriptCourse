20e0042039dfc3dcd10c81d51f7dc46e6514d739
Richard <ricky.cary1@gmail.com>
2023-04-13 18:39:39 -0500

    Initial Commit

---
+++ 01-Fundamentals-Part-1/fundamentals.js
@@ -0,0 +1,18 @@
+// Values and Variables
+let country = 'USA';
+let continent = 'North America';
+let population = '15'
+console.log(country)
+console.log(continent)
+console.log(population)
+
+// Data Types
+let isIsland = "Hawaii";
+let language;
+
+console.log(isIsland);
+console.log(typeof isIsland);
+console.log(language);
+console.log(typeof country);
+console.log(typeof continent);
+console.log(typeof population);
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,40 @@
+// let js = 'amazing';
+
+// console.log(40 + 8 + 23 - 10);
+
+// console.log('Jonas');
+// console.log(23);
+
+// -------------- Values and Variables ---------------//
+// let firstName = "Ricky";
+
+// console.log(firstName);
+
+// let myFirstJob = "Programmer";
+// let myCurrentJob = "Nurse";
+
+// console.log(myFirstJob)
+// console.log(myCurrentJob)
+
+//
+// Data Types 
+//
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Ricky');
+
+javascriptIsFun = 'Yes!';
+
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(year)
