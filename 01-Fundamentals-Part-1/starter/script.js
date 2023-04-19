6b5725448fa51b2c2c6a0dd1a5852a619081de72
Richard <ricky.cary1@gmail.com>
2023-04-19 15:24:39 -0500

    Truthy and Falsy Values

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -141,3 +141,17 @@ console.log(Boolean('Ricky'));
 console.log(Boolean({}));
 console.log(Boolean(NaN));
 console.log(Boolean(''));
+
+const money = 100;
+if (money) {
+    console.log("Don't spend it all")
+} else {
+    console.log("You should get a job!")
+}
+
+let height = 1;
+if (height) {
+    console.log("Yay! Height is defined!");
+} else {
+    console.log("Height is UNDEFINED!")
+}
