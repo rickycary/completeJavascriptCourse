c7fe28ae933090cab56d5f34576f9afac6f5d70d
Richard <ricky.cary1@gmail.com>
2023-04-19 16:05:36 -0500

    Equality Operators

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -135,23 +135,44 @@
 
 // -------------- Truthy and Falsy Values ---------------//
 // 5 falsy values: 0, '', undefined, null, NaN 
-console.log(Boolean(0));
-console.log(Boolean(undefined));
-console.log(Boolean('Ricky'));
-console.log(Boolean({}));
-console.log(Boolean(NaN));
-console.log(Boolean(''));
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean('Ricky'));
+// console.log(Boolean({}));
+// console.log(Boolean(NaN));
+// console.log(Boolean(''));
 
-const money = 100;
-if (money) {
-    console.log("Don't spend it all")
+// const money = 100;
+// if (money) {
+//     console.log("Don't spend it all")
+// } else {
+//     console.log("You should get a job!")
+// }
+
+// let height = 1;
+// if (height) {
+//     console.log("Yay! Height is defined!");
+// } else {
+//     console.log("Height is UNDEFINED!")
+// }
+
+// -------------- Equality Operators ---------------//
+const age = '18'; 
+if (age === 18) console.log("You just became and adult :D (strict)")
+// Two equals signs has type coercion and loose equality
+if (age == 18) console.log("You just became and adult :D (loose)")
+// Use strict equality with ===
+
+const favorite = Number(prompt("What's your favorite number?"));
+console.log(favorite)
+console.log(typeof favorite)
+
+if (favorite === 23) {
+    console.log("Cool! 23 is an amazing number!")
+} else if (favorite === 7) {
+    console.log("7 is also a cool number!")
 } else {
-    console.log("You should get a job!")
+    console.log("Incorrect as the only right answer is 7 and 23")
 }
 
-let height = 1;
-if (height) {
-    console.log("Yay! Height is defined!");
-} else {
-    console.log("Height is UNDEFINED!")
-}
+if (favorite !== 23) console.log("Why not 23?")
