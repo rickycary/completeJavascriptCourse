76f1c91b481c7049edc5b43faa4cc7477ae5c5f6
Richard <ricky.cary1@gmail.com>
2023-04-21 17:08:07 -0500

    Fundamentals: The Conditional Operator

--- 01-Fundamentals-Part-1/starter/fundamentals.js
+++ 01-Fundamentals-Part-1/starter/fundamentals.js
@@ -69,24 +69,28 @@
 //     console.log("Portugal does not meet your criteria...")
 // }
 
-const language = 'english'
-switch (language) {
-    case 'chinese':
-    case 'mandarin':
-        console.log("Most number of native speakers")
-        break;
-    case 'spanish':
-        console.log("2nd place in number of native speakers")
-        break;
-    case 'english':
-        console.log("3rd place");
-        break;
-    case 'hindi':
-        console.log("4th place");
-        break;
-    case 'arabic':
-        console.log("5th place");
-        break;
-    default:
-        console.log("Great language too!")
-}
+// const language = 'english'
+// switch (language) {
+//     case 'chinese':
+//     case 'mandarin':
+//         console.log("Most number of native speakers")
+//         break;
+//     case 'spanish':
+//         console.log("2nd place in number of native speakers")
+//         break;
+//     case 'english':
+//         console.log("3rd place");
+//         break;
+//     case 'hindi':
+//         console.log("4th place");
+//         break;
+//     case 'arabic':
+//         console.log("5th place");
+//         break;
+//     default:
+//         console.log("Great language too!")
+// }
+
+population = 32;
+
+population > 33 ? console.log("Portugal's population is above average.") : console.log("Portugal's population is below average.")
