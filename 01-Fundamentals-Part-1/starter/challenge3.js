2224b1f085123de76101723473aed2b11a4addb4
Richard <ricky.cary1@gmail.com>
2023-04-21 16:21:30 -0500

    Coding Challenge 3: Bonus 1

--- 01-Fundamentals-Part-1/starter/challenge3.js
+++ 01-Fundamentals-Part-1/starter/challenge3.js
@@ -1,13 +1,28 @@
-scoreDolphins = (96 + 108 + 89) / 3
-scoreKoalas = (88 + 91 + 10) / 3
+// scoreDolphins = (96 + 108 + 89) / 3
+// scoreKoalas = (88 + 91 + 10) / 3
+
+// console.log(scoreDolphins, scoreKoalas)
+
+// if (scoreDolphins > scoreKoalas) {
+//     console.log("The Dolphins have the higher average score!")
+// } else if (scoreKoalas > scoreDolphins) {
+//     console.log("The Koalas have the higher average score!")
+// } else if (scoreDolphins === scoreKoalas) {
+//     console.log("Both win the trophy")
+// };
+
+// Bonus 1
+scoreDolphins = (97 + 112 + 101) / 3;
+scoreKoalas = (109 + 95 + 123) / 3;
 
 console.log(scoreDolphins, scoreKoalas)
-
-if (scoreDolphins > scoreKoalas) {
+if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
     console.log("The Dolphins have the higher average score!")
-} else if (scoreKoalas > scoreDolphins) {
+} else if (scoreKoalas > scoreDolphins && scoreKoalas >= 100) {
     console.log("The Koalas have the higher average score!")
 } else if (scoreDolphins === scoreKoalas) {
     console.log("Both win the trophy")
 };
 
+
+// Bonus 2 
