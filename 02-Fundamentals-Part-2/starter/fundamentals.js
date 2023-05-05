fa751ec92d0e1c6de450d78327dab9c43d004473
Richard <ricky.cary1@gmail.com>
2023-05-05 15:47:02 -0500

    Fundamentals: Functions calling other functions

--- 02-Fundamentals-Part-2/starter/fundamentals.js
+++ 02-Fundamentals-Part-2/starter/fundamentals.js
@@ -4,21 +4,21 @@
 //Functions
 ///////////////////
 
-// function describeCountry(country, population, capitalCity){
-//     console.log(country, population, capitalCity);
-//     const returnString =  `${country} has ${population} million people and it's capital city is ${capitalCity}.`
-//     return returnString;
-// }
-// console.log(describeCountry)
+function describeCountry(country, population, capitalCity){
+    console.log(country, population, capitalCity);
+    const returnString =  `${country} has ${population} million people and it's capital city is ${capitalCity}.`
+    return returnString;
+}
+console.log(describeCountry)
 
-// const callFunction1 = describeCountry("Finland", 6, "Helsinki")
-// const callFunction2 = describeCountry("US", 8, "Washington DC")
-// const callFunction3 = describeCountry("Mexico", 2, "Juarez")
+const callFunction1 = describeCountry("Finland", 6, "Helsinki")
+const callFunction2 = describeCountry("US", 8, "Washington DC")
+const callFunction3 = describeCountry("Mexico", 2, "Juarez")
 
 
-// console.log(callFunction1)
-// console.log(callFunction2)
-// console.log(callFunction3)
+console.log(callFunction1)
+console.log(callFunction2)
+console.log(callFunction3)
 
 
 ///////////////////
@@ -39,9 +39,9 @@ const country3 = precentageOfWorld2(100)
 
 console.log(country1, country2, country3)
 
-///////////////////
+/////////////////
 // Arrow Functions
-///////////////////
+/////////////////
 
 const precentageOfWorld3 = (population) => {
     return (population / 7900) * 100;
@@ -49,3 +49,15 @@ const precentageOfWorld3 = (population) => {
 const country4 = precentageOfWorld1(1000);
 
 console.log(country4)
+
+
+///////////////////
+// Functions calling other functions
+///////////////////
+const describePopulation = (country, population) => {
+    const percentage =  precentageOfWorld1(population)
+    const description = `${country} has ${population} million people, which is about ${percentage}% of the world.`;
+    console.log(description);
+};
+
+describePopulation('USA', 10)
