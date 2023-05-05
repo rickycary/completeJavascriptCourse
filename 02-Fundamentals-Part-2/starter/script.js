11c82ebea1109d8a50c1e8625c8a6e0d0f72d2a1
Richard <ricky.cary1@gmail.com>
2023-05-04 21:38:41 -0500

    Functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,10 +1,34 @@
 // Use Strict mode
 'use strict'
-let hasDriversLicense = false; 
-const passTest = true; 
+// let hasDriversLicense = false; 
+// const passTest = true; 
 
-if (passTest) hasDriversLicense = true; 
-if (hasDriversLicense) console.log("I can drive!")
+// if (passTest) hasDriversLicense = true; 
+// if (hasDriversLicense) console.log("I can drive!")
 
 // const interface = 'Audio'
 // const private = 534; 
+
+/////////////////////////////////
+// Functions
+/////////////////////////////////
+function logger() {
+    console.log("My name is Ricky");
+}
+// Calling / Running / Invoking the function 
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice; 
+}
+
+const appleJuice = fruitProcessor(3, 4);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
