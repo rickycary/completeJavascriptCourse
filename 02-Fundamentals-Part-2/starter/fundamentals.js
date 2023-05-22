4be72a951ad9b396868f498e4aafbdf7ecab9188
Richard <ricky.cary1@gmail.com>
2023-05-22 10:15:30 -0500

    Fundamentals: Looping Backwards and Loops in Loops

--- 02-Fundamentals-Part-2/starter/fundamentals.js
+++ 02-Fundamentals-Part-2/starter/fundamentals.js
@@ -125,10 +125,20 @@
 // Looping Arrays, Breaking and Continuing 
 ///////////////////
 
-const populations = [10, 1441, 332, 83]; 
-const percentages2 = [];
-for (let i = 0; i < populations.length; i++) {
-    const perc = percentageOfWorld1(populations[i]);
-    percentages2.push(perc);
-}
-console.log(percentages2)
+// const populations = [10, 1441, 332, 83]; 
+// const percentages2 = [];
+// for (let i = 0; i < populations.length; i++) {
+//     const perc = percentageOfWorld1(populations[i]);
+//     percentages2.push(perc);
+// }
+// console.log(percentages2)
+
+///////////////////
+// Looping Backwards and Loops in Loops
+///////////////////
+
+const listOfNeighbors = [
+    ['Canada', 'Mexico'],
+    ['Spain'],
+    ['Norway', 'Sweden', 'Russia']    
+]
